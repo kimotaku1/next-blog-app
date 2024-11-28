3918189ea3fdfe34b1a809b734dc5681e1624f54
Your Name <you@example.com>
2024-11-28 08:30:18 +0545

    updated

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
+module.exports = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {}, // Adds vendor prefixes for better browser support
   },
 };
-
-export default config;
